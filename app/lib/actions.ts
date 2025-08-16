d88df20b65e3b31ded86934bc5419c9e0cbfb811
biruk fekadu <birukefekadu632@gmail.com>
2025-08-16 22:13:53 +0300

    adding aria labels, add form validation to edit-form file

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
 
+
 'use server';
 import {z} from 'zod';
 import { revalidatePath } from 'next/cache';
@@ -91,3 +92,39 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function updateInvoice(
+  id:string,
+  prevState: State,
+  formData: FormData,
+){
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    }
+  };
+  
+ 
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+ 
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
